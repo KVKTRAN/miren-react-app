8915d30848c996018548b59624e235feec20a23d
k v <tkvkhang28122000@gmail.com>
2022-05-28 22:42:49 -0400

    Updates

---
+++ precache-manifest.7d1e3ac7a75d270b61a87587e742682e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e116adb8e61c0706089a8565554e5d",
+    "url": "/miren-react-app/index.html"
+  },
+  {
+    "revision": "7aaa134e34bc0c4d1888",
+    "url": "/miren-react-app/static/css/main.dc3b7e38.chunk.css"
+  },
+  {
+    "revision": "e0bc53f349012c544969",
+    "url": "/miren-react-app/static/js/2.93a7f13e.chunk.js"
+  },
+  {
+    "revision": "87d162f166c41b843ff0",
+    "url": "/miren-react-app/static/js/3.f3e739f5.chunk.js"
+  },
+  {
+    "revision": "7aaa134e34bc0c4d1888",
+    "url": "/miren-react-app/static/js/main.cf1aa8d2.chunk.js"
+  },
+  {
+    "revision": "cace97da939f158edc88",
+    "url": "/miren-react-app/static/js/runtime~main.4a1e232a.js"
+  }
+]);
